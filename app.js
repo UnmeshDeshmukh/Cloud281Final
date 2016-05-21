4f9d92a11c75b224bc7fd527dbdfb72263f0368d
UnmeshDeshmukh <unmesh.deshmukh@sjsu.edu>
2016-05-21 15:28:53 -0700

    Node Gumball Codes

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var page = function( req, res, state ) {
                     var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
                     res.end( html_body );
             });
-}
+}*/
 
 var order = function(req, res) {
     var client = new Client();
@@ -66,7 +66,7 @@ var order = function(req, res) {
                         } 
                     );
             });
-}*/
+}
 
 var handle_post = function (req, res) {
     console.log( "Post: " + "Action: " +  req.body.event + " State: " + req.body.state + "\n" ) ;
@@ -88,19 +88,20 @@ var handle_post = function (req, res) {
     }    
 }
 
-/*var handle_get = function (req, res) {
+var handle_get = function (req, res) {
     console.log( "Get: ..." ) ;
     page( req, res, "no-coin" ) ;
-}*/
+}
 
 app.set('port', (process.env.PORT || 5000));
 
 app.post("*", handle_post );
-//app.get( "*", handle_get ) ;
+app.get( "*", handle_get ) ;
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+}
+);
 
 
 
