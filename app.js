5f782c4b1d991e530ec9356da0fa040aa11410f8
Unmesh Deshmukh <unmesh.deshmukh@sjsu.edu>
2016-05-25 10:47:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,208 @@
+var endpoint = "http://52.9.120.14:8080/GrailsGumballMachineVer2-2.0/gumballs/1";
+
+
+// added in v2: crypto
+// crypto functions:  http://nodejs.org/api/crypto.html
+
+var crypto = require('crypto');
+var fs = require('fs');
+var express = require('express');
+var Client = require('node-rest-client').Client;
+
+var app = express();
+app.use(express.bodyParser());
+app.use("/images", express.static(__dirname + '/images'));
+
+var secretKey = "kwRg54x2Go9iEdl49jFENRM12Mp711QI" ;
+
+var get_hash = function( state, ts ) {
+
+    // http://nodejs.org/api/crypto.html#crypto_crypto_createhmac_algorithm_key
+    text = state + "|" + ts + "|" + secretKey ;
+    hmac = crypto.createHmac("sha256", secretKey);
+    hmac.setEncoding('base64');
+    hmac.write(text);
+    hmac.end() ;
+    hash = hmac.read();
+    //console.log( "HASH: " + hash )
+    return hash ;
+
+}
+
+
+var error = function( req, res, msg, ts ) {
+
+    body = fs.readFileSync('./gumball.html');
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+
+    state = "error" ;
+    hash = get_hash ( state, ts ) ;
+
+    var html_body = "" + body ;
+    var html_body = html_body.replace("{message}", msg );
+    var html_body = html_body.replace("{ts}", ts );
+    var html_body = html_body.replace("{hash}", hash );
+    var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
+    res.end( html_body );
+}
+
+
+
+var page = function( req, res, state, ts ) {
+
+    body = fs.readFileSync('./gumball.html');
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+
+    hash = get_hash ( state, ts ) ;
+    console.log( state ) ;
+
+    var client = new Client();
+            var count = "";
+            client.get( endpoint, 
+                function(data, response_raw){
+                    console.log(data);
+                    count = data.countGumballs
+                    console.log( "count = " + count ) ;
+                    var msg =   "\n\nMighty Gumball, Inc.\n\nNodeJS-Enabled Standing Gumball\nModel# " + 
+                                data.modelNumber + "\n" +
+                                "Serial# " + data.serialNumber + "\n" +
+                                "\n" + state +"\n\n" ;
+                    var html_body = "" + body ;
+                    var html_body = html_body.replace("{message}", msg );
+                    var html_body = html_body.replace("{ts}", ts );
+                    var html_body = html_body.replace("{hash}", hash );
+                    var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
+                    res.end( html_body );
+            });
+}
+
+var order = function( req, res, state, ts ) {
+
+    var client = new Client();
+            var count = 0;
+            client.get( endpoint, 
+                function(data, response_raw) {
+                    count = data.countGumballs ;
+                    console.log( "count before = " + count ) ;
+                    if ( count > 0 ) {
+                        count-- ;
+                        var args = {
+                            data: {  "countGumballs": count, },
+                            headers:{"Content-Type": "application/json"} 
+                        };
+                        client.put( endpoint, args,
+                            function(data, response_raw) {
+                                console.log(data);
+                                console.log( "count after = " + data.countGumballs ) ;
+                                page( req, res, state, ts ) ;
+                            } 
+                        );
+                    }
+                    else {
+                        error( req, res, "*** OUT OF INVENTORY ***", ts ) ;
+                    }
+            });
+}
+
+
+var handle_post = function (req, res) {
+
+    console.log( "Post: " + "Action: " +  req.body.event + " State: " + req.body.state + "\n" ) ;
+    var hash1 = "" + req.body.hash ;
+    var state = "" + req.body.state ;
+    var action = "" + req.body.event ;
+    var ts = parseInt(req.body.ts) ;
+    var now = new Date().getTime() ;
+    var diff = ((now - ts)/1000) ;
+    hash2 = get_hash ( state, ts ) ;
+    console.log( "DIFF:  " +  diff ) ;
+    console.log( "HASH1: " + hash1 ) ;
+    console.log( "HASH2: " + hash2 ) ;
+
+    if ( diff > 120 || hash1 != hash2 ) {
+        error( req, res, "*** SESSION INVALID ***", ts ) ;
+    }
+    else if ( action == "Insert Quarter" ) {
+        if ( state == "no-coin" )
+            page( req, res, "has-coin", ts ) ;
+        else
+            page( req, res, state, ts ) ;
+            
+    }
+    else if ( action == "Turn Crank" ) {
+        if ( state == "has-coin" ) {
+            hash = get_hash ( "no-coin", ts ) ;
+            order(req, res, "no-coin", ts ) ;
+        }
+        else
+            page( req, res, state, ts ) ;
+    }  
+  
+}
+
+var handle_get = function (req, res) {
+    console.log( "Get: ..." ) ;
+
+    ts = new Date().getTime()
+    console.log( ts )
+    state = "no-coin" ;
+
+   // page( req, res, state, ts ) ;
+}
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+
+var gett = function (req, res){
+
+
+var fs = require("fs");
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://52.24.136.109:27017,52.35.94.222:27017,52.33.102.9:27017/test?w=0&readPreference=secondary";
+
+
+
+    MongoClient.connect(url, function(err, db) {
+        var cursor =db.collection('mycol').find();
+        var doc_total = {};
+        var index = 0;
+        cursor.each(function(err, doc) {
+          console.log("Doc is "+doc)
+          if (doc != null) {
+             doc_total[index++] = doc;
+          } else {
+              db.close();
+              res.end(JSON.stringify((doc_total),null,4));
+          }
+       });
+    });
+}       
+
+
+//app.set('port',5000);
+app.set('port', (process.env.PORT || 5000));
+
+app.post("*", handle_post );
+//app.get( "*", handle_get ) ;
+app.get("/test",gett)
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+On Sat, May 21, 2016 at 5:53 PM, Vinit Gaikwad <vinit.gaikwad10@gmail.com> wrote:
+
 /**
 
 Mighty Gumball, Inc.
@@ -164,7 +369,7 @@ var handle_get = function (req, res) {
     console.log( ts )
     state = "no-coin" ;
 
-  //  page( req, res, state, ts ) ;
+   // page( req, res, state, ts ) ;
 }
 
 var user = {
@@ -178,14 +383,34 @@ var user = {
 
 
 var gett = function (req, res){
-data={};
- data["user4"] = user["user4"];
-       console.log( data );
-       res.end( JSON.stringify(data));
-}
+
+
+var fs = require("fs");
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var url =
+ "mongodb://52.38.239.149:27017,52.37.63.167:27017,52.37.105.177:27017/test?w=0&readPreference=secondary";
 
 
 
+    MongoClient.connect(url, function(err, db) {
+        var cursor =db.collection('mycol').find();
+        var doc_total = {};
+        var index = 0;
+        cursor.each(function(err, doc) {
+          console.log("Doc is "+doc)
+          if (doc != null) {
+             doc_total[index++] = doc;
+          } else {
+              db.close();
+              res.end(JSON.stringify((doc_total),null,4));
+          }
+       });
+    });
+}       
+
+
+//app.set('port',5000);
 app.set('port', (process.env.PORT || 5000));
 
 app.post("*", handle_post );
